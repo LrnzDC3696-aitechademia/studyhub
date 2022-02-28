4be9b1af3ba0318009e256882c3290d0f1fd5c31
m0nk3ybraindead <zyphr.euch@gmail.com>
2022-02-28 12:10:59 +0800

    traversy media templates done

--- base/views.py
+++ base/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+
+
+rooms = [
+    {
+        'id': 1,
+        'name': 'This is room 1'
+    },
+    {
+        'id': 2,
+        'name': 'This is room 2'
+    },
+    {
+        'id': 3,
+        'name': 'This is room 3'
+    }
+]
 
 
 def home(request):
-    return render(request, 'home.html')
+    context = {
+        'rooms': rooms
+    }
+    return render(request, 'base/home.html', context=context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+            break
+    context = {
+        'room': room
+    }
+    return render(request, 'base/room.html', context=context)
