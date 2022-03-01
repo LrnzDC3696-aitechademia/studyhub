88c9a895ab48641b195961a6e1d82a082a174847
m0nk3ybraindead <zyphr.euch@gmail.com>
2022-03-01 17:20:32 +0800

    traversy media user logout login done

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='base-home'),
+    path("login/", views.login_page, name="base-login"),
+    path("logout/", views.logout_page, name="base-logout"),
     path('room/<str:pk>', views.room, name='base-room'),
     path("create_room/", views.create_room, name="base-create_room"),
     path("update_room/<str:pk>", views.update_room, name="base-update_room"),
--- base/views.py
+++ base/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from django.db.models import Q
+from django.contrib import messages
+from django.contrib.auth.models import User
 from .models import Room, Topic
 from .forms import RoomForm
+
+
+def logout_page(request):
+    logout(request)
+    return render(request, "base/login_register.html")
+
+
+def login_page(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:  # type: ignore
+            messages.error(request, "user does not exist")
+            return render(request, "base/login_register.html", context)
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("base-home")
+        else:
+            messages.error(request, "invalid credentials")
+
+    return render(request, "base/login_register.html", context)
 
 
 def home(request):
