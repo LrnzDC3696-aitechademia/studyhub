bbd00c29af281862540be9e07b127df468b09e58
m0nk3ybraindead <zyphr.euch@gmail.com>
2022-03-04 00:02:12 +0800

    traversy media theme installation done

--- base/views.py
+++ base/views.py
@@ -124,14 +124,22 @@ def delete_message(request, pk):
 @login_required(login_url="base-login")
 def create_room(request):
     form = RoomForm()
+    topics = Topic.objects.all()
+
     if request.method == "POST":
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect("base-home")
-    context = {"form": form}
+        topic_name = request.POST.get("topic")
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get("name"),
+            description=request.POST.get("description"),
+        )
+
+        return redirect("base-home")
+
+    context = {"form": form, 'topics': topics}
     return render(request, "base/room_form.html", context)
 
 
@@ -139,17 +147,25 @@ def create_room(request):
 def update_room(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
 
     if request.user != room.host:
         return HttpResponse("You are not allowed to edit this room")
 
     if request.method == "POST":
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect("base-home")
+        topic_name = request.POST.get("topic")
+        topic, created = Topic.objects.get_or_create(name=topic_name)
 
-    context = {"form": form}
+        # form = RoomForm(request.POST, instance=room)
+        # if form.is_valid():
+        #     form.save()
+        room.name = request.POST.get("name")
+        room.topic = topic
+        room.description = request.POST.get("description")
+        room.save()
+        return redirect("base-home")
+
+    context = {"form": form, 'topics': topics, 'room': room}
     return render(request, "base/room_form.html", context)
 
 
